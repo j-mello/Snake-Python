33d3a1538d498f133f9f80ea434305c9beef021c
j-mello <mello.julien@gmail.com>
2021-12-30 22:05:37 +0100

    Ajout du fruit, du serpent et des commandes de déplacement et d'agrandissement

--- snake.py
+++ snake.py
@@ -1,22 +1,103 @@
-import pygame, sys
-
+import pygame, sys, random
+from pygame.math import Vector2
 from pygame import surface
 
 pygame.init()
+cell_size = 40
+cell_number = 20
 
-screen = pygame.display.set_mode((800,800))
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5,10), Vector2(6,10),Vector2(7,10)]
+        self.direction = Vector2(1,0)
+        self.new_block = False
+
+    def draw_snake(self):
+        for block in self.body:
+            #create a rectangle
+            x_pos = block.x*cell_size
+            y_pos = block.y*cell_size
+            block_rect = pygame.Rect(x_pos, y_pos,cell_size,cell_size)
+            #draw the rectangle
+            pygame.draw.rect(screen,(252,36,236),block_rect)
+
+    def move_snake(self):
+        if self.new_block == True:
+            body_copy = self.body[:]
+            body_copy.insert(0,body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0,body_copy[0] + self.direction)
+            self.body = body_copy[:]
+
+    def add_block(self):
+        self.new_block = True
+
+class FRUIT:
+    def __init__(self):
+        self.randomize()
+
+    def draw_fruit(self):
+        #create the rectangle
+        fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
+        #draw the rectangle
+        pygame.draw.rect(screen,(126,166,114), fruit_rect)
+
+    def randomize(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x,self.y)
+
+class MAIN:
+    def __init__(self):
+        self.snake = SNAKE()
+        self.fruit = FRUIT()
+    
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()
+
+    def draw_elements(self):
+        self.snake.draw_snake()
+        self.fruit.draw_fruit()
+    
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]: #Si le fruit et la tête du serpent sont à la même position
+            # replacer un fruit ailleurs
+            self.fruit.randomize()
+            # rendre le serpent plus grand
+            self.snake.add_block()
+
+main_game = MAIN()
+
+
+screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((400,100))
-test_surface.fill(pygame.Color('blue'))
-test_rectangle = test_surface.get_rect(center = (400,400))
+
+SCREEN_UPDATE = pygame.USEREVENT #creation d'un event
+pygame.time.set_timer(SCREEN_UPDATE, 150) #L'event sera trigger toutes les 150 millisecondes
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    # screen.fill(pygame.Color('gold'))
+        if event.type == SCREEN_UPDATE:
+            main_game.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                main_game.snake.direction = Vector2(0,-1)
+            if event.key == pygame.K_DOWN:
+                main_game.snake.direction = Vector2(0,1)
+            if event.key == pygame.K_RIGHT:
+                main_game.snake.direction = Vector2(1,0)
+            if event.key == pygame.K_LEFT:
+                main_game.snake.direction = Vector2(-1,0)
+    
     screen.fill((175,215,70))
-    test_rectangle.right += 1
-    screen.blit(test_surface, test_rectangle)
+    main_game.draw_elements()
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(60) #La boucle s'exécute 60 fois par seconde, correspondant aux 60 FPS
