82a00a02a5478b8da829fa06cad82e7996bb574e
j-mello <mello.julien@gmail.com>
2022-01-01 11:07:06 +0100

    Ajout graphismes et conditions game over basique

--- snake.py
+++ snake.py
@@ -3,23 +3,94 @@ from pygame.math import Vector2
 from pygame import surface
 
 pygame.init()
+
+clock = pygame.time.Clock()
 cell_size = 40
 cell_number = 20
+screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
+apple = pygame.image.load('graphisms/apple.png').convert_alpha()
 
 class SNAKE:
     def __init__(self):
-        self.body = [Vector2(5,10), Vector2(6,10),Vector2(7,10)]
+        self.body = [Vector2(5,10), Vector2(4,10),Vector2(3,10)]
         self.direction = Vector2(1,0)
         self.new_block = False
 
+        # Graphismes pour la tête du serpent
+        self.head_up = pygame.image.load('graphisms/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load('graphisms/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load('graphisms/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load('graphisms/head_left.png').convert_alpha()
+
+        # Graphismes pour la queue du serpent
+        self.tail_up = pygame.image.load('graphisms/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('graphisms/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load('graphisms/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load('graphisms/tail_left.png').convert_alpha()
+
+        # Graphismes pour le corps du serpent
+        self.body_vertical = pygame.image.load('graphisms/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('graphisms/body_horizontal.png').convert_alpha()
+
+        # Graphismes pour le corps du serpent en train de tourner
+        self.body_tr = pygame.image.load('graphisms/body_topright.png').convert_alpha()
+        self.body_tl = pygame.image.load('graphisms/body_topleft.png').convert_alpha()
+        self.body_br = pygame.image.load('graphisms/body_bottomright.png').convert_alpha()
+        self.body_bl = pygame.image.load('graphisms/body_bottomleft.png').convert_alpha()
+
+
     def draw_snake(self):
-        for block in self.body:
-            #create a rectangle
-            x_pos = block.x*cell_size
-            y_pos = block.y*cell_size
-            block_rect = pygame.Rect(x_pos, y_pos,cell_size,cell_size)
-            #draw the rectangle
-            pygame.draw.rect(screen,(252,36,236),block_rect)
+        self.update_head_graphisms()
+        self.update_tail_graphisms()
+
+        for index, block in enumerate(self.body):
+            # On crée les rect pour les positions
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size,cell_size)
+
+            # Tête du serpent
+            if index == 0:
+                screen.blit(self.head,block_rect)
+            # Queue du serpent
+            elif index == len(self.body) - 1 :
+                screen.blit(self.tail,block_rect)
+            # Corps du serpent
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x :
+                    screen.blit(self.body_vertical,block_rect)
+                elif previous_block.y == next_block.y :
+                    screen.blit(self.body_horizontal,block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1 :
+                        screen.blit(self.body_tl, block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1 :
+                        screen.blit(self.body_bl, block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1 :
+                        screen.blit(self.body_tr, block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1 :
+                        screen.blit(self.body_br, block_rect)
+    
+    def update_head_graphisms(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == Vector2(1,0): self.head = self.head_left
+        elif head_relation == Vector2(-1,0): self.head = self.head_right
+        elif head_relation == Vector2(0,1): self.head = self.head_up
+        elif head_relation == Vector2(0,-1): self.head = self.head_down
+
+    def update_tail_graphisms(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1,0): self.tail = self.tail_left
+        elif tail_relation == Vector2(-1,0): self.tail = self.tail_right
+        elif tail_relation == Vector2(0,1): self.tail = self.tail_up
+        elif tail_relation == Vector2(0,-1): self.tail = self.tail_down
+
+
+
+
+
 
     def move_snake(self):
         if self.new_block == True:
@@ -44,7 +115,8 @@ class FRUIT:
         #create the rectangle
         fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
         #draw the rectangle
-        pygame.draw.rect(screen,(126,166,114), fruit_rect)
+        screen.blit(apple, fruit_rect)
+        #pygame.draw.rect(screen,(126,166,114), fruit_rect)
 
     def randomize(self):
         self.x = random.randint(0, cell_number - 1)
@@ -59,6 +131,7 @@ class MAIN:
     def update(self):
         self.snake.move_snake()
         self.check_collision()
+        self.check_fail()
 
     def draw_elements(self):
         self.snake.draw_snake()
@@ -70,12 +143,21 @@ class MAIN:
             self.fruit.randomize()
             # rendre le serpent plus grand
             self.snake.add_block()
+    
+    def check_fail(self):
+        #Si snake est en dehors de l'écran
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            self.game_over()
+        #Si snake se touche lui-même
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.game_over()
+
+    def game_over():
+        pygame.quit()
+        sys.exit()
 
 main_game = MAIN()
-
-
-screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
-clock = pygame.time.Clock()
 
 SCREEN_UPDATE = pygame.USEREVENT #creation d'un event
 pygame.time.set_timer(SCREEN_UPDATE, 150) #L'event sera trigger toutes les 150 millisecondes
@@ -89,13 +171,17 @@ while True:
             main_game.update()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                main_game.snake.direction = Vector2(0,-1)
+                if main_game.snake.direction.y != 1:
+                    main_game.snake.direction = Vector2(0,-1)
             if event.key == pygame.K_DOWN:
-                main_game.snake.direction = Vector2(0,1)
+                if main_game.snake.direction.y != -1:
+                    main_game.snake.direction = Vector2(0,1)
             if event.key == pygame.K_RIGHT:
-                main_game.snake.direction = Vector2(1,0)
+                if main_game.snake.direction.x != -1:
+                    main_game.snake.direction = Vector2(1,0)
             if event.key == pygame.K_LEFT:
-                main_game.snake.direction = Vector2(-1,0)
+                if main_game.snake.direction.x != 1:
+                    main_game.snake.direction = Vector2(-1,0)
     
     screen.fill((175,215,70))
     main_game.draw_elements()
